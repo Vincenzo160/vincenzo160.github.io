3a626b29806ade38ed1efdd04419848e3a084c24
Vincenzo Deluca <delucaavincenzo@gmail.com>
2023-08-25 00:30:15 +0200

    ultimate fix

--- scripts/theme-handler.js
+++ scripts/theme-handler.js
@@ -1,10 +1,11 @@
 var currentTheme = document.getElementsByTagName("meta")[0].content
 var savedTheme = document.cookie.slice(6)
 
-if (!(currentTheme === savedTheme)) {
-    setTheme(savedTheme)
-} else if (savedTheme === "") {
+if (savedTheme === "") {
+    console.log("aaaa")
     setTheme("sgt")
+} else if (!(currentTheme === savedTheme)) {
+    setTheme(savedTheme)
 }
 
 let selectTheme = document.querySelector(".theme-selector");
@@ -18,10 +19,12 @@ function setCookie(theme) {
     document.cookie = "theme="+ theme +"; expires=Thu, 18 Dec 9999 12:00:00 UTC; path=/";
 }
 function setTheme(theme) {
-    setCookie(theme)
+    console.log(theme)
     if (theme === "sgt") {
+        setCookie("sgt")
         location.replace("/")
     } else {
+        setCookie(theme)
         location.replace("/themes/"+theme)
     }
 }
