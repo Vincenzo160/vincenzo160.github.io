03d17b0d0d1be0b8a9b53db50694fe65b0222af2
Vincenzo Deluca <delucaavincenzo@gmail.com>
2023-08-24 23:25:17 +0200

    Persistent themes

--- scripts/theme-handler.js
+++ scripts/theme-handler.js
@@ -1,11 +1,33 @@
+var currentTheme = document.getElementsByTagName("meta")[0].content
+var savedTheme = document.cookie.slice(6)
+
+if (currentTheme === savedTheme) {
+    
+} else {
+    setTheme(savedTheme)
+}
+
 let selectTheme = document.querySelector(".theme-selector");
 
 selectTheme.addEventListener("change", (event) => {
     var selection = event.target.value
     if (selection == "theme-sgt") {
-        location.replace("/")
+        setTheme(selection.slice(6))
     } else if (selection == "theme-glass") {
-        location.replace("/themes/glass.html")
+        setTheme(selection.slice(6))
     }
 })
 
+function setCookie(theme) {
+    document.cookie = "theme="+ theme +"; expires=Thu, 18 Dec 9999 12:00:00 UTC; path=/";
+}
+function setTheme(theme) {
+    console.log(theme)
+    if (theme === "sgt") {
+        setCookie("sgt")
+        location.replace("/")
+    } else {
+        setCookie(theme)
+        location.replace("/themes/"+theme+".html")
+    }
+}
