9fab56b10f679a8f8c1f487bc96e901b0876c1b3
Vincenzo Deluca <delucaavincenzo@gmail.com>
2023-08-24 23:49:48 +0200

    updated theme-handler.js

--- scripts/theme-handler.js
+++ scripts/theme-handler.js
@@ -1,9 +1,7 @@
 var currentTheme = document.getElementsByTagName("meta")[0].content
 var savedTheme = document.cookie.slice(6)
 
-if (currentTheme === savedTheme || savedTheme === "") {
-    
-} else {
+if (!(currentTheme === savedTheme || savedTheme === "")) {
     setTheme(savedTheme)
 }
 
